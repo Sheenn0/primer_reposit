9ef57036feadd1864f29227a5f56a353b3f5d603
Sheenn0 <sheenn0@yahoo.com.mx>
2025-10-01 08:17:41 -0700

    Create programa_practica.py

---
+++ programa_practica.py
@@ -0,0 +1,6 @@
+# Este es un script de Python de prueba para el repositorio
+def saludo(nombre):
+  return "Hola "+nombre
+
+# se invoca a la funcion saludo para saludar a alguien llamado Jovi
+saludo("Jovi")
